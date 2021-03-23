928f806b114212c76e9ea6d54901c8118947319d
Katsiaryna Zhukouskaya <K.Zhukouskaya@itechart-group.com>
2021-03-23 14:25:01 +0300

    env variable rem

--- Scripts/FollowersCheckCase/Script1616493170324.groovy
+++ Scripts/FollowersCheckCase/Script1616493170324.groovy
@@ -1,14 +1,12 @@
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import jenkins.model.Jenkins
 
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
-def envVars = Jenkins.instance.getGlobalNodeProperties()[0 as String].getEnvVars()
-expectedAmount = envVars['count']
+
 
 response = WS.sendRequest(findTestObject('User/Get Followers', [('variable') : '', ('variable_0') : '', ('variable_1') : '']))
 
 WS.verifyResponseStatusCode(response, 200)
 
-WS.verifyElementPropertyValue(response, 'response.count', expectedAmount)
+WS.verifyElementPropertyValue(response, 'response.count', 39988)
 
